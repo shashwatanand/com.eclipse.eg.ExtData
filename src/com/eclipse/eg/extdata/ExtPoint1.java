2c8cace49a0cf43a227b31ea9c76384c0afec467
Shashwat Anand <shashwat.anand@gmail.com>
2015-01-12 19:14:03 +0530

    Initail Commit

---
+++ src/com/eclipse/eg/extdata/ExtPoint1.java
@@ -0,0 +1,17 @@
+package com.eclipse.eg.extdata;
+
+import com.eclipse.eg.extrcp.extPoint.IExtPoint;
+
+public class ExtPoint1 implements IExtPoint {
+
+	public ExtPoint1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.getClass().getName();
+	}
+
+}
---
+++ src/com/eclipse/eg/extdata/ExtPoint2.java
@@ -0,0 +1,17 @@
+package com.eclipse.eg.extdata;
+
+import com.eclipse.eg.extrcp.extPoint.IExtPoint;
+
+public class ExtPoint2 implements IExtPoint {
+
+	public ExtPoint2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.getClass().getName();
+	}
+
+}
